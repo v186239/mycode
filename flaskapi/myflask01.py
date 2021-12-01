c1e38c8b3f18f7d944d3cee2d16f1738108d7563
Will Rivera <william.rivera@verizonwireless.com>
2021-12-01 22:06:54 +0000

    My first Flask apps

--- flaskapi/myflask01.py
+++ flaskapi/myflask01.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # route() function of the Flask class is a
 # decorator, tells the application which URL
 # should call the associated function
-@app.route("/")
+@app.route("/hello")
 def hello_world():
    return "Hello World"
 
